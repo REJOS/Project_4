db05b8e7901d4f6e62b87007f6f78f58244d9b2c
root <root@localhost.localdomain>
2017-04-16 22:10:49 -0400

    All methods in file_syscalls.c filled out

--- src/kern/userprog/file_syscalls.c
+++ src/kern/userprog/file_syscalls.c
@@ -21,18 +21,110 @@
 
 typedef int mode_t;
 
-int sys_open(const char *path, int oflag, mode_t mode) {
-	return 0;
+int sys_open(const char *path, int oflag) {
+	int * retfd;
+	file_open(path,oflag,retfd);
+	return *retfd;
 }
 
 
 int sys_read(int fd, void *buf, size_t nbytes) {
 
-	return 0;
+	int result=0;
+	if(fd >= OPEN_MAX || filehandle < 0){
+		return EBADF;
+	}
+
+	struct openfile **file;
+	filetable_findfile(fd,**file);
+	if(*file == NULL){
+		return EBADF;
+	}
+
+	if(*file->of_accmode == O_WRONLY){
+		return EBADF;
+	}
+
+	void *kbuf;
+	kbuf = kmalloc(sizeof(*buf)*nbytes);
+	if(kbuf == NULL){
+		return EINVAL;
+	}
+
+	struct iovec v;
+	struct uio u;
+
+	lock_acquire(*file->of_lock);
+	v.iov_ubase = (userptr_t)buf;
+	v.iov_len = nbytes;
+	u.uio_iov=&v;
+	u.uio_iovcnt = 1;
+	u.uio_offset = *file->of_offset;
+	u.uio_resid = nbytes;
+	u.uio_segflg = UIO_USERSPACE;
+	u.uio_rw = UIO_READ;
+	u.uio_space = curthread->t_addrspace;
+
+	result = VOP_READ(*file->of_vnode,&u);
+	if(result){
+		kfree(kbuf);
+		lock_release(*file->of_lock);
+		return result;
+	}
+	*file->of_offset = u.uio_offset;
+	result = nbytes - u.uio_resid;
+	kfree(kbuf);
+	lock_release(*file->of_lock);
+	return result;
 }
 
 
 int sys_write(int fd, const void *buf, size_t nbytes) {
+	int result = 0;
+	if(fd >= OPEN_MAX || fd < 0){
+		return EBADF;
+	}
+	struct openfile **file;
+	filetable_findfile(fd,**file);
+	if(*file == NULL){
+		return EBADF;
+	}
+
+	if(*file->of_accmode == O_RDONLY){
+		return EBADF;
+	}
+
+	void *kbuf;
+		kbuf = kmalloc(sizeof(*buf)*nbytes);
+		if(kbuf == NULL){
+			return EINVAL;
+		}
+
+		struct iovec v;
+		struct uio u;
+
+		lock_acquire(*file->of_lock);
+		v.iov_ubase = (userptr_t)buf;
+		v.iov_len = nbytes;
+		u.uio_iov=&v;
+		u.uio_iovcnt = 1;
+		u.uio_offset = *file->of_offset;
+		u.uio_resid = nbytes;
+		u.uio_segflg = UIO_USERSPACE;
+		u.uio_rw = UIO_READ;
+		u.uio_space = curthread->t_addrspace;
+
+		result = VOP_WRITE(*file->of_vnode,&u);
+		if(result){
+			kfree(kbuf);
+			lock_release(*file->of_lock);
+			return result;
+		}
+		*file->of_offset = u.uio_offset;
+		result = nbytes - u.uio_resid;
+		kfree(kbuf);
+		lock_release(*file->of_lock);
+		return result;
 
 	return 0;
 
@@ -40,7 +132,68 @@ int sys_write(int fd, const void *buf, size_t nbytes) {
 
 
 int sys_lseek(int fd, off_t offset, int whence) {
+	int result = 0;
 
+	struct openfile **file;
+	filetable_findfile(fd,**file);
+
+	if(fd >= OPEN_MAX || fd < 0){
+		return EBADF;
+	}
+
+	if(*file == NULL){
+		return EBADF;
+	}
+
+	off_t pos, file_size;
+	struct stat statbuf;
+
+	lock_acquire(*file->of_lock);
+	result = VOP_STAT(*file->of_vnode, &statbuf);
+	if(result){
+		lock_release(*file->of_lock);
+		return result;
+	}
+	file_size = statbuf.st_size;
+
+	if(whence == SEEK_SET){
+		result = VOP_TRYSEEK(*file->of_vnode,offset);
+		if(result){
+			lock_release(*file->of_lock);
+			return result;
+		}
+		pos = offset;
+	}
+
+	else if(whence == SEEK_CUR){
+		result = VOP_TRYSEEK(*file->of_vnode,*file->of_offset + offset);
+		if(result){
+			lock_release(*file->of_lock);
+			return result;
+		}
+		pos = *file->of_offset+offset;
+	}
+
+	else if(whence == SEEK_END){
+		result = VOP_TRYSEEK(*file->of_vnode,file_size+offset);
+		if(result){
+			lock_release(*file->of_lock);
+			return result;
+		}
+		pos = file_size + offset;
+	}
+	else{
+		lock_release(*file->of_lock);
+		return EINVAL;
+	}
+
+	if(pos < (off_t)0){
+		lock_release(*file->of_lock);
+		return EINVAL;
+	}
+	*file->offset = pos;
+
+	lock_release(*file->of_lock);
 
 
 	return 0;
@@ -48,16 +201,10 @@ int sys_lseek(int fd, off_t offset, int whence) {
 
 
 int sys_close(int fd) {
-
-
-
-	return 0;
+	return file_close(fd);
 }
 
 
 int sys_dup2file(int oldfd, int newfd) {
-
-
-
-	return 0;
+	return filetable_dup2file(oldfd, newfd);
 }
